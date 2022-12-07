ad47cc06856bd04dd26748f802434cf06435e367
kiana <66397580+kianayao@users.noreply.github.com>
2022-12-06 20:41:38 -0800

    Update Node.java

--- 3560_proj_2/src/Node.java
+++ 3560_proj_2/src/Node.java
@@ -1,6 +1,7 @@
 public class Node implements Comparable<Node>{
     protected String _id;
-
+    protected long _creationTime;
+    
     @Override
     public int compareTo(Node node){
         return _id.compareTo(node._id);
